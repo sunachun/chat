6b7a04bc7b1c1372da9e5c6257d93a83abfcae8c
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-06 17:50:55 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
