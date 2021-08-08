72ad39fb1aea31d1278cb96ae28008b819c57329
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-08 12:27:28 +0900

    319

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,23 +5,31 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works!'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/RRveQLQ5E6NiK1zNXvLt/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                documents[index]['text'],
+              ),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/RRveQLQ5E6NiK1zNXvLt/messages')
-              .snapshots()
-              .listen((data) {
-            print(data);
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
