fba5e6fdd6ea345445ee69fc79a3735bab1e18a9
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-09 18:14:54 +0900

    340 image picker

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -48,16 +48,19 @@ class _AuthScreenState extends State<AuthScreen> {
 
         await ref.putFile(image).onComplete;
 
+        final url = await ref.getDownloadURL();
+
         await Firestore.instance
             .collection('users')
             .document(authResult.user.uid)
             .setData({
           'username': username,
           'email': email,
+          'image_url': url,
         });
       }
     } on PlatformException catch (err) {
-      var message = 'An error occured, please check your credentials!';
+      var message = 'An error occurred, please check your credentials!';
 
       if (err.message != null) {
         message = err.message;
