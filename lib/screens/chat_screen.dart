b72b6df44771c2f6db6ca50b0aa9f428e1b645e5
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-10 14:43:36 +0900

    347 firebase_messaging

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,24 @@
 import 'package:chat/widgets/chat/messages.dart';
 import 'package:chat/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
