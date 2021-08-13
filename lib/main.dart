23abc67eab596ade60d88c0b08995ed0a0765c7b
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-13 12:15:05 +0900

    354 finish

--- lib/main.dart
+++ lib/main.dart
@@ -14,40 +14,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final Future<FirebaseApp> _initialization = Firebase.initializeApp();
     return FutureBuilder(
-        // Initialize FlutterFire:
-        future: _initialization,
-        builder: (context, appSnapshot) {
-          return MaterialApp(
-            title: 'FlutterChat',
-            theme: ThemeData(
-              primarySwatch: Colors.pink,
-              backgroundColor: Colors.pink,
-              accentColor: Colors.deepPurple,
-              accentColorBrightness: Brightness.dark,
-              buttonTheme: ButtonTheme.of(context).copyWith(
-                buttonColor: Colors.pink,
-                textTheme: ButtonTextTheme.primary,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, appSnapshot) {
+        return MaterialApp(
+          title: 'FlutterChat',
+          theme: ThemeData(
+            primarySwatch: Colors.pink,
+            backgroundColor: Colors.pink,
+            accentColor: Colors.deepPurple,
+            accentColorBrightness: Brightness.dark,
+            buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.pink,
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
               ),
             ),
-            home: appSnapshot.connectionState != ConnectionState.done
-                ? SplashScreen()
-                : StreamBuilder(
-                    stream: FirebaseAuth.instance.authStateChanges(),
-                    builder: (ctx, userSnapshot) {
-                      if (userSnapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return SplashScreen();
-                      }
-                      if (userSnapshot.hasData) {
-                        return ChatScreen();
-                      }
-                      return AuthScreen();
-                    },
-                  ),
-          );
-        });
+          ),
+          home: appSnapshot.connectionState != ConnectionState.done
+              ? SplashScreen()
+              : StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (ctx, userSnapshot) {
+                    if (userSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return SplashScreen();
+                    }
+                    if (userSnapshot.hasData) {
+                      return ChatScreen();
+                    }
+                    return AuthScreen();
+                  },
+                ),
+        );
+      },
+    );
   }
 }
